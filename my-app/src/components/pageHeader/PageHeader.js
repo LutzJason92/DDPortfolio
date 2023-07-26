0cc745a8524caf1b9b16f56e44831b8e94b19eb7
Jason <LutzJason92@gmail.com>
2023-07-26 16:04:02 -0600

    RED DOTTTTTTTTTTTT

---
+++ my-app/src/components/pageHeader/PageHeader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./pageHeader.css";
+
+// import compnents from material Ui - style later in custom css
+
+export default function PageHeader() {
+  return (
+    // mobile version tabs sticky bottom
+    // header and Page title on top
+    <div className="header">
+      <h1>JASON DANIEL LUTZ</h1>
+    </div>
+
+    // tab 1
+    // tab 2
+    // tab 3
+    // tab 4
+  );
+}
